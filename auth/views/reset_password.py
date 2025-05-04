b9c81b1772bb685915fb952b6d20463ce2c28e74
Egor1158 <egor115819@gmail.com>
2025-05-04 20:47:11 +0300

    The process of registration, autorization and account confirmation has been modernized

---
+++ auth/views/reset_password.py
@@ -0,0 +1,47 @@
+import flask, Project
+
+from .view_send_email import list_code
+from .view_sing_up import user_data, list_code_account
+from flask_login import current_user, login_user
+from ..models import User
+
+def render_reset_app():
+
+    if flask.request.method == "POST":
+        
+        code = int(flask.request.form['code'])
+        if code == list_code[-1]:
+            return flask.redirect(location = '/../new_password')
+        
+    return flask.render_template('reset_password.html')
+
+
+def render_confirm_account():
+    if flask.request.method == "POST":
+        code = int(flask.request.form['code'])
+        if code == list_code_account[-1]:
+                
+            user = User(
+                name = user_data['name'],
+                email = user_data["email"],
+                password = user_data['password'],
+                password_confirmation = user_data['password_confirmation'],
+                is_teacher = bool(user_data['is_teacher']),
+                is_certified = True
+            )                   
+            
+            Project.db.session.add(user)
+            Project.db.session.commit()
+
+            login_user(user)
+            
+            return flask.redirect(location = '/../')
+    
+    if not current_user.is_authenticated:
+        return flask.render_template(template_name_or_list="reset_password.html")
+    else:
+        return flask.redirect('/')
+
+
+
+
